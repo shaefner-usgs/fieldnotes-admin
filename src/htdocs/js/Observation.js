4e27cfcd6ab983d62dcce86eb55f96b333cf1a51
Scott Haefner <shaefner@usgs.gov>
2016-05-06 16:24:15 -0700

    add option go get a formatted string

--- src/htdocs/js/Observation.js
+++ src/htdocs/js/Observation.js
@@ -21,12 +21,32 @@ var Observation = function (options) {
     _this.set(options);
   };
 
-  _this.getCoords = function () {
-    var geometry;
+  _this.getCoords = function (format) {
+    var coords,
+        lat,
+        lon,
+        geometry,
+        str,
+        z;
 
     geometry = _this.get('geometry') || {};
+
     if (geometry.hasOwnProperty('coordinates')) {
-      return geometry.coordinates;
+      if (format === 'string') {
+        coords = geometry.coordinates;
+        lat = [Math.abs(coords[1]).toFixed(2), '&deg;',
+          (coords[1] < 0 ? 'S':'N')].join('');
+        lon = [Math.abs(coords[0]).toFixed(2), '&deg;',
+          (coords[0] < 0 ? 'W':'E')].join('');
+        z = coords[2] + 'm';
+        str = lat + ', ' + lon;
+        if (coords[2]) {
+          str += ' (' + z + ')';
+        }
+        return str;
+      } else {
+        return geometry.coordinates;
+      }
     }
 
     return null;
