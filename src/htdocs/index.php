0eaf53731f50c3a8cbe91bb0e83ff021c066be9e
Scott Haefner <shaefner@usgs.gov>
2016-05-10 12:27:01 -0700

    use relative links to leaflet

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -10,13 +10,13 @@ include_once '../conf/config.inc.php'; // app config
   <title>Fieldnotes</title>
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css" />
+  <link rel="stylesheet" href="lib/leaflet-0.7.7/leaflet.css" />
   <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
   <div class="application"></div>
   <script>var MOUNT_PATH = '<?php print $MOUNT_PATH; ?>';</script>
-  <script src="/lib/leaflet-0.7.7/leaflet.js"></script>
+  <script src="lib/leaflet-0.7.7/leaflet.js"></script>
   <script src="js/index.js"></script>
 </body>
 </html>
